c48ee68882ed3b3735b003e2f69113fbc39120e2
dohaianh <dha190320022@gmail.com>
2021-02-20 10:06:32 +0700

    4nd commit

--- index.js
+++ index.js
@@ -21,13 +21,23 @@ var nobtn = document.getElementById("noBut");
 function Move(){
     var posX = Math.random()* 1000-700;
     var posY = Math.random()* 300-100;
+    if(window.innerWidth < 600){
+        posX = Math.random()*100-Math.random()*300;
+        posY = Math.random()*70 - Math.random()*200;
+    }
     nobtn.style.transform = `translate3d(${posX}px, ${posY}px, 0px)`
 }
 
 nobtn.addEventListener("mousemove" , function(){
     Move();
 })
+console.log(window.innerWidth)
 var a = document.getElementsByClassName("a");
+var b = document.getElementsByClassName("b");
+console.log(a, b)
 if (window.innerWidth < 600){
-    a.style.fontSize = "10px";
+    for (var i=0; i < 2; i++){
+        a[i].style.fontSize = "20px";
+        b[i].style.fontSize = "10px";
+    }
 }
